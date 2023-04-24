30f74bc93547ffd1f1606375ee661c5278c28cd2
iamitpal <amitpalinfo@yahoo.in>
2023-04-25 02:06:22 +0530

    carousel and pagination

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Update order to Delivered
+//@route GET /api/orders/:id/deliver
+//@access Private/admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 //@desc get Logged in user orders
 //@route GET /api/orders/myorders
 //@access Private
@@ -87,4 +106,19 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+//@desc get all orders
+//@route GET /api/orders/
+//@access Private/admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+  updateOrderToDelivered,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,10 +6,14 @@ import {
   getProducts,
   getProductsById,
   updateProduct,
+  createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
 
 router
   .route("/:id")
